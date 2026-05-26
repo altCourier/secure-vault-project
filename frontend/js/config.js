a5c54c8fd52498fc1d4c6b62d40d0148c16037dd
altCourier <szehra_uluyol@outlook.com>
2026-05-26 18:28:56 +0300

    fix: missing https:// line

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
 // For local machine:
 // const API_BASE = "http://localhost:3000";
 
-const API_BASE = "secure-vault-project-production.up.railway.app";
+const API_BASE = "https://secure-vault-project-production.up.railway.app";
