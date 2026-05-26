ee3b43adf0d71b209347d643bb746476e2201ab5
altCourier <szehra_uluyol@outlook.com>
2026-05-26 19:21:00 +0300

    test: initial test of create tables

---
+++ database/initial_tests_show_Tables.sql
@@ -0,0 +1,10 @@
+SHOW TABLES;
+
+DESCRIBE Audit_Log;
+DESCRIBE Users;
+DESCRIBE Credentials;
+DESCRIBE User_Session;
+DESCRIBE Security_State;
+DESCRIBE User_MFA_Factors;
+DESCRIBE Recovery_Codes;
+DESCRIBE MFA_Methods;
